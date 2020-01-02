9cc854c95cbabf922e21c89967fe8b28aa28f614
Onur Tuna <onurtuna@icloud.com>
2020-01-02 20:24:00 +0300

    project created

---
+++ News MVVM-C/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  News MVVM-C
+//
+//  Created by Onur Tuna on 2.01.2020.
+//  Copyright © 2020 Onur Tuna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

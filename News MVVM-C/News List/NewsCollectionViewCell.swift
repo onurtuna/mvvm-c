b9c835324cec375e2a3f50928650514bf2a6c36c
Onur Tuna <onurtuna@icloud.com>
2020-01-02 21:10:18 +0300

    news collection cell

---
+++ News MVVM-C/News List/NewsCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  NewsCollectionViewCell.swift
+//  News MVVM-C
+//
+//  Created by Onur Tuna on 2.01.2020.
+//  Copyright © 2020 Onur Tuna. All rights reserved.
+//
+
+import UIKit
+
+class NewsCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ News MVVM-C/News List/NewsListViewController.swift
@@ -0,0 +1,30 @@
+//
+//  NewsListViewController.swift
+//  News MVVM-C
+//
+//  Created by Onur Tuna on 2.01.2020.
+//  Copyright © 2020 Onur Tuna. All rights reserved.
+//
+
+import UIKit
+
+class NewsListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
